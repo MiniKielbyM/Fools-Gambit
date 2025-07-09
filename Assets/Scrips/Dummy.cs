948ea2012fc1d921c3d9987d671d9bb8f8227534
standpoint456 <standpoint09@gmail.com>
2025-07-09 22:46:29 +0300

    fuck you maciain you  and your tutorial code

---
+++ Assets/Scrips/Dummy.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Dummy : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float health = 100;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void minushealth()
+    {
+        health -= 50;
+    }
+}
---
+++ Assets/Scrips/hitdummy.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class hitdummy : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created#
+    public float range = 100;
+    public Camera cam;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            hit();
+            Debug.Log ("shot");
+        }
+    }
+    void hit()
+    {
+        RaycastHit hit;
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit, range))
+        {
+            GameObject hitGameObject = hit.transform.gameObject;
+            if (hitGameObject == gameObject.CompareTag("dummy"))
+            {
+                hitGameObject.GetComponent<Dummy>().minushealth();
+                Debug.Log("hit");
+            }
+
+        }
+
+    }
+    public void OnAttack(InputAction.CallbackContext context)
+    {
+            hit();
+            Debug.Log("shot");   
+    }
+}
