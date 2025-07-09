a301e3cbce2b34547db7d08fe6c0a451cdd6d057
MiniKielbyM <85803354+MiniKielbyM@users.noreply.github.com>
2025-07-09 15:04:14 -0400

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- Assets/PlayerControl.cs
+++ Assets/PlayerControl.cs
@@ -1,125 +1,102 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-using System.Collections;
 
 public class PlayerControl : MonoBehaviour
 {
-    public float mouseSensitivity = 0.1f;
-    public float moveSpeed = 5f;
-    public float verticalClampMin = -45f;
-    public float verticalClampMax = 70f;
-    public float bobFrequency = 8f;
-    public float bobAmplitude = 0.05f;
-    public bool veiwBob;
+    [Header("Movement")]
+    public float moveSpeed;
+    public float groundDrag;
+    public float jumpForce;
+    public float jumpCooldown;
+    public float airMultiplier;
+    private bool readyToJump = true;
 
-    public Transform modelBody;
-    public Rigidbody playerRigidbody;
-    public Animator playerAnimator;
-    public Animation landingAnimation;
-    public Transform CameraTransform;
+    [HideInInspector] public float walkSpeed;
+    [HideInInspector] public float sprintSpeed;
 
-    private Transform playerBody;
-    private PlayerInput playerInput;
-    private InputAction lookAction;
-    private InputAction moveAction;
-    private InputAction jumpAction;
+    [Header("Ground Check")]
+    public float playerHeight;
+    public LayerMask whatIsGround;
+    private bool grounded;
 
-    private Vector3 originalCameraLocalPos;
-    private float bobTimer = 0f;
-    private float verticalRotation = 0f;
+    public Transform orientation;
 
-    void Start()
+    private Vector2 moveInput;
+    private Vector3 moveDirection;
+
+    private Rigidbody rb;
+
+    private void Start()
     {
-        playerBody = GetComponent<Transform>();
-
-        playerInput = GetComponent<PlayerInput>();
-        lookAction = playerInput.actions["Look"];
-        moveAction = playerInput.actions["Move"];
-        jumpAction = playerInput.actions["Jump"];
-        Cursor.lockState = CursorLockMode.Locked;
-
-        originalCameraLocalPos = CameraTransform.localPosition;
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
     }
 
-    void Update()
+    private void Update()
     {
-        // --- Look ---
-        Vector2 lookInput = lookAction.ReadValue<Vector2>();
-        float mouseX = lookInput.x * (mouseSensitivity / 10f);
-        float mouseY = lookInput.y * (mouseSensitivity / 10f);
+        Debug.DrawRay(transform.position, Vector3.down * (playerHeight * 0.5f + 0.3f), Color.red);
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.3f, whatIsGround);
 
-        // Horizontal rotation (player yaw)
-        playerBody.Rotate(Vector3.up * mouseX);
+        SpeedControl();
 
-        // Vertical rotation (camera pitch)
-        verticalRotation -= mouseY;
-        verticalRotation = Mathf.Clamp(verticalRotation, verticalClampMin, verticalClampMax);
-        CameraTransform.localRotation = Quaternion.Euler(verticalRotation, 0f, 0f);
+        // Set drag based on grounded state
+        rb.linearDamping = grounded ? groundDrag : 0;
+    }
 
-        // --- Movement ---
-        Vector2 moveInput = moveAction.ReadValue<Vector2>();
-        float threshold = 0.1f;
-        Vector2 dir = moveInput.normalized;
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
 
-        if (moveInput.magnitude < threshold && IsGrounded())
+    // These methods get linked in the Unity Inspector via PlayerInput Unity Events
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        Debug.Log("Jump Input Received: " + context.performed);
+        if (context.performed && readyToJump && grounded)
         {
-            if (playerAnimator.GetInteger("AnimState") == 10) { }
-            playerAnimator.SetInteger("AnimState", 0);
+            readyToJump = false;
+            Jump();
+            Invoke(nameof(ResetJump), jumpCooldown);
         }
-        else if (Approximately(dir, new Vector2(0, 1)) && IsGrounded())
-            playerAnimator.SetInteger("AnimState", 1);
-        else if (Approximately(dir, new Vector2(0, -1)) && IsGrounded())
-            playerAnimator.SetInteger("AnimState", 2);
-        else if (Approximately(dir, new Vector2(-1, 0)) && IsGrounded())
-            playerAnimator.SetInteger("AnimState", 3);
-        else if (Approximately(dir, new Vector2(1, 0)) && IsGrounded())
-            playerAnimator.SetInteger("AnimState", 4);
-        else if (Approximately(dir, new Vector2(0.71f, 0.71f)) && IsGrounded())
-            playerAnimator.SetInteger("AnimState", 6);
-        else if (Approximately(dir, new Vector2(-0.71f, 0.71f)) && IsGrounded())
-            playerAnimator.SetInteger("AnimState", 5);
-        else if (Approximately(dir, new Vector2(0.71f, -0.71f)) && IsGrounded())
-            playerAnimator.SetInteger("AnimState", 8);
-        else if (Approximately(dir, new Vector2(-0.71f, -0.71f)) && IsGrounded())
-            playerAnimator.SetInteger("AnimState", 7);
-        else if (!IsGrounded())
-            playerAnimator.SetInteger("AnimState", 10);
+    }
+    public void OnAttack(InputAction.CallbackContext context)
+    {
 
-        // Jump
-        if (jumpAction.triggered && IsGrounded())
-        {
-            playerAnimator.SetInteger("AnimState", 9);
-            playerRigidbody.AddForce(Vector3.up * 5f, ForceMode.Impulse);
-        }
+    }
+    private void MovePlayer()
+    {
+        moveDirection = orientation.forward * moveInput.y + orientation.right * moveInput.x;
 
-        // Apply Movement
-        playerBody.position += playerBody.forward * moveInput.y * moveSpeed * Time.deltaTime;
-        playerBody.position += playerBody.right * moveInput.x * moveSpeed * Time.deltaTime;
-        modelBody.position = playerBody.position;
-
-        // --- Head Bob ---
-        if (veiwBob && moveInput.magnitude > 0.1f && IsGrounded())
-        {
-            bobTimer += Time.deltaTime * bobFrequency;
-            float bobOffset = Mathf.Sin(bobTimer) * bobAmplitude;
-            Vector3 newCamPos = originalCameraLocalPos + new Vector3(0, bobOffset, 0);
-            CameraTransform.localPosition = newCamPos;
-        }
+        if (grounded)
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
         else
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+
+        if (flatVel.magnitude > moveSpeed)
         {
-            bobTimer = 0;
-            CameraTransform.localPosition = Vector3.Lerp(CameraTransform.localPosition, originalCameraLocalPos, Time.deltaTime * 10f);
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.linearVelocity = new Vector3(limitedVel.x, rb.linearVelocity.y, limitedVel.z);
         }
     }
 
-    private bool IsGrounded()
+    private void Jump()
     {
-        Debug.DrawRay(modelBody.position, Vector3.down * 0.05f, Color.red);
-        return Physics.Raycast(modelBody.position + new Vector3(0, 0.05f, 0), Vector3.down, 0.1f);
+        rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
     }
 
-    private bool Approximately(Vector2 a, Vector2 b)
+    private void ResetJump()
     {
-        return Vector2.Distance(a, b) < 0.05f;
+        readyToJump = true;
     }
 }
--- Assets/hitdummy.cs
+++ Assets/hitdummy.cs
@@ -4,7 +4,7 @@ public class hitdummy : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created#
     public float range = 100;
-    public Camera camera;
+    public Camera cam;
     void Start()
     {
 
@@ -13,15 +13,11 @@ public class hitdummy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0)){
-            hit();
-            Debug.Log("shot");
-        }
     }
     void hit()
     {
         RaycastHit hit;
-        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
 
         if (Physics.Raycast(ray, out hit, range))
         {
